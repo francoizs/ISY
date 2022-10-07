e92e17779b03a6db9793ddab4a4e321c346001df
Ihab Safadi <69370301+IhabSaf@users.noreply.github.com>
2022-10-07 16:30:32 +0200

    Add files via upload

---
+++ Project ISY-periode1/GameFramework/Main.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Board newboard = new Board(3,3);
+        System.out.println(newboard);
+
+    }
+
+}
