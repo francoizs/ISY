3e06cf3744b5a21179d0c968135f98534afe5ec0
Ihab Safadi <69370301+IhabSaf@users.noreply.github.com>
2022-10-08 01:47:05 +0200

    Update Board.java

--- Project ISY-periode1/GameFramework/Board.java
+++ Project ISY-periode1/GameFramework/Board.java
@@ -73,9 +73,31 @@ public class Board{
      * Adds a move to the board.
      */
 
-    public void add(){
+    public int[] coordinate(int position){
+         /**
+         * // Deze methode neemt de position die de speler wil doen, en geeft de coordinates op het board terug.
+         * Vervolgens  worden de coordinates gestuurd naar de add methode om een zet te doen op het board
+         */
+        int counter =  0;
+        int[] coordinate= {0,0};
+        for (int row=0; row< this.height; row++){
+            for (int col = 0; col< this.width; col++){
+                counter ++;
+                if (counter == position){coordinate[0] =row; coordinate[1] = col;break;}
+            }
+        }
 
+        return coordinate;
     }
+    
+    public void add(char teken, int position){
+        /**
+         * voeg een zet toe aan het baord
+         */
+                int[] coordinates = coordinate(position);
+                if(board[coordinates[0]][coordinates[1]] ==' '){board[coordinates[0]][coordinates[1]] = teken;}
+                else {System.out.println("Dit plek is niet beschikbaar");}
+            }
 
     /**
      * Removes a move from the board.
