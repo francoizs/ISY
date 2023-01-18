06e3ae6bb989ad648be86f4b194ccd5378ead0f1
martdevries <101712463+martdevries@users.noreply.github.com>
2023-01-18 20:10:07 +0100

    More data and auto testomgeving

--- ISY-main/projectAIperoide1enTwee/gameFramework/Main.java
+++ ISY-main/projectAIperoide1enTwee/gameFramework/Main.java
@@ -6,6 +6,8 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Project intelligente Systemen
@@ -18,57 +20,257 @@ public class Main{
     public static void main(String[] args) throws IOException {
 
 //        Gui gui = new Gui();
-        int potjes = 0;
-        while (potjes < 10) {
-            Othello othello = new Othello(8, 8, '•');
-            othello.startPositions();
-            Player player1 = new Player(1, '◦', "AI");
-            Player player2 = new Player(2, '•', "AI");
+        int manualorauto = 0;
 
-            potjes ++;
-            while (!othello.isFull()) {
+        if (manualorauto == 0) {
+            int player1win = 0;
+            int draw = 0;
+            int player2win = 0;
+            int potjes = 0;
+            while (potjes < 2) {
+                Othello othello = new Othello(8, 8, '•');
+                othello.startPositions();
+                Player player1 = new Player(1, '◦', "AI");
+                Player player2 = new Player(2, '•', "AI");
 
-                //Player1
-                AiOthello ai = new AiOthello(player1.getPlayernumber(), player1.getPiece());
-                int move1 = ai.AiMove(othello, player1.getPiece(), 1);
-                if (move1 != 0) {
-                    while (!othello.allowMoveOthello(move1, (player1.getPiece()))) {
+                potjes++;
+                while (!othello.isFull()) {
+                    for (int i = 0; i < 64; i++) {
+                        if (othello.allowMoveOthello(i, player1.getPiece())) {
+                            //Player1
+                            // check if player 1 has thought of a move in 10 seconds
+                            long startTime = System.currentTimeMillis();
+                            AiOthello ai = new AiOthello(player1.getPlayernumber(), player1.getPiece());
+                            int move1 = ai.AiMove(othello, player1.getPiece(), 9);
+                            long endTime = System.currentTimeMillis();
+                            long duration = (endTime - startTime);
+                            if (duration > 10000) {
+                                System.out.println("Player 1 took too long to think of a move");
+                                break;
+                            }
+                            othello.add(player1.getPiece(), move1);
+                            othello.flipPiece(move1, player1.getPiece());
+                            System.out.println(othello.toString());
+                            break;
+                        }
+                    }
 
-                        move1 = ai.AiMove(othello, player1.getPiece(), 1);
+                    for (int b = 0; b < 64; b++) {
+                        if (othello.allowMoveOthello(b, player2.getPiece())) {
+                            // player2
+                            long startTime2 = System.currentTimeMillis();
+                            AiOthello ai2 = new AiOthello(player2.getPlayernumber(), player2.getPiece());
+                            int move2 = ai2.AiMove(othello, player2.getPiece(), 1);
+                            long endTime2 = System.currentTimeMillis();
+                            long duration2 = (endTime2 - startTime2);
+                            if (duration2 > 10000) {
+                                System.out.println("Player 2 took too long to think of a move");
+                                break;
+                            }
+                            othello.add(player2.getPiece(), move2);
+                            othello.flipPiece(move2, player2.getPiece());
+                            System.out.println(othello.toString());
+                            break;
+                        }
+                    }
+
+                    ArrayList<Integer> lijst = new ArrayList<>();
+                    for (int c = 0; c < 64; c++) {
+                        if (othello.allowMoveOthello(c, player1.getPiece()) || othello.allowMoveOthello(c, player2.getPiece())) {
+                            lijst.add(0, 1);
+                        }
+                    }
+                    if (lijst.size() == 0) {
+                        break;
                     }
                 }
-                othello.add(player1.getPiece(), move1);
-                othello.flipPiece(move1, player1.getPiece());
-                System.out.println(othello.toString());
 
-                // player2
-                AiOthello ai2 = new AiOthello(player2.getPlayernumber(), player2.getPiece());
-                int move2 = ai2.AiMove(othello, player2.getPiece(), 1);
-                if (move2 != 0) {
-                    while (!othello.allowMoveOthello(move2, player2.getPiece())) {
+                Path fileName = Path.of("/ResultsAi/TestAivsGreedyAi.txt"); //path van het bestand waarin de data wordt opgeslagen.
 
-                        move2 = ai2.AiMove(othello, player2.getPiece(), 1);
-                    }
+                BufferedWriter writer = new BufferedWriter(new FileWriter(fileName.toFile(), true));
+                writer.append("\n");
+                String ply1 = String.valueOf("Player: " + player1.getPlayernumber() + " Score is: " + othello.pieceCounter(player1.getPiece()));
+                String ply2 = String.valueOf("Player: " + player2.getPlayernumber() + " Score is: " + othello.pieceCounter(player2.getPiece()));
+                String result = ply1 + " || " + ply2 + " || nr_potje: " + potjes;
+
+                if (othello.pieceCounter(player1.getPiece()) > othello.pieceCounter(player2.getPiece())) {
+                    player1win++;
+                } else if (othello.pieceCounter(player1.getPiece()) == othello.pieceCounter(player2.getPiece())) {
+                    draw++;
+                } else {
+                    player2win++;
                 }
-                othello.add(player2.getPiece(), move2);
-                othello.flipPiece(move2, player2.getPiece());
-                System.out.println(othello.toString());
+
+                if (potjes == 1) {
+                    writer.append("----------------------------------------------------------------");
+                    writer.append("\n");
+                    writer.append("Begin test");
+                    writer.append("\n");
+                    writer.append("\n");
+                    writer.append("Player 1: TestAi");
+                    writer.append("\n");
+                    writer.append("Player 2: GreedyAi");
+                    writer.append("\n");
+                    writer.append("Aantal potjes: 100");
+                    writer.append("\n");
+                    writer.append("Max depth: 4");
+                    writer.append("\n");
+                    writer.append("\n");
+                }
+
+                writer.append(result);
+
+                if (potjes == 2) {
+                    writer.append("\n");
+                    writer.append("\n");
+                    writer.append("Player 1 total wins: ").append(String.valueOf(player1win));
+                    writer.append("\n");
+                    writer.append("Total draws: ").append(String.valueOf(draw));
+                    writer.append("\n");
+                    writer.append("Player 2 total wins: ").append(String.valueOf(player2win));
+                    writer.append("\n");
+                    writer.append("\n");
+                    writer.append("Einde test");
+                    writer.append("\n");
+                    writer.append("----------------------------------------------------------------");
+                    writer.append("\n");
+                }
+
+                writer.close();
             }
+        } else {
+            ArrayList<String> ai4 = new ArrayList<>();
+            Collections.addAll(ai4, "GreedyAi", "GreedyAiWP", "GenerousAi", "GenerousAiWP", "GreedyMovesAi", "GreedyMovesAiWP", "TilePointsAi", "TilePointsAiWP");
+            for (String q : ai4) {
+                for (int s = 0; s < 2; s++) {
+                    int player1win = 0;
+                    int draw = 0;
+                    int player2win = 0;
+                    int potjes = 0;
+                    while (potjes < 100) {
+                        Othello othello = new Othello(8, 8, '•');
+                        othello.startPositions();
+                        Player player1 = new Player(1, '◦', "AI");
+                        Player player2 = new Player(2, '•', "AI");
 
-            Path fileName = Path.of("/ResultsAi/result.txt"); //path van het bestand waarin de data wordt opgeslagen.
+                        potjes++;
+                        while (!othello.isFull()) {
+                            for (int i = 0; i < 64; i++) {
+                                if (othello.allowMoveOthello(i, player1.getPiece())) {
+                                    //Player1
+                                    if (s == 0) {
+                                        AiOthello ai = new AiOthello(player1.getPlayernumber(), player1.getPiece());
+                                        int move1 = ai.AiMove(othello, player1.getPiece(), 9);
+                                        othello.add(player1.getPiece(), move1);
+                                        othello.flipPiece(move1, player1.getPiece());
+                                        System.out.println(othello.toString());
+                                        break;
+                                    } else {
+                                        AiOthello ai = new AiOthello(player1.getPlayernumber(), player1.getPiece());
+                                        int move1 = ai.AiMove(othello, player1.getPiece(), ai4.indexOf(q)+1);
+                                        othello.add(player1.getPiece(), move1);
+                                        othello.flipPiece(move1, player1.getPiece());
+                                        System.out.println(othello.toString());
+                                        break;
+                                    }
+                                }
+                            }
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName.toFile(), true));
-            writer.append("\n");
-            String ply1 = String.valueOf("Player: " + player1.getPlayernumber() + " Score is: " + othello.pieceCounter(player1.getPiece()));
-            String ply2 = String.valueOf("Player: " + player2.getPlayernumber() + " Score is: " + othello.pieceCounter(player2.getPiece()));
-            String result = ply1 + " || " + ply2 + " || nr_potje: " + potjes;
-            if (potjes == 1){writer.append("\n");writer.append("Begin test");writer.append("\n");}
-//            if (potjes == 1){writer.append("\n");writer.append("Begin");writer.append("\n");}
-//            if (potjes == 50){writer.append("\n");writer.append("Mid");writer.append("\n");}
-//            if (potjes == 80){writer.append("\n");writer.append("Einde");writer.append("\n");}
-            if (potjes == 100){writer.append("\n");writer.append("Einde test");writer.append("\n");}
-            else {writer.append(result);}
-            writer.close();
+                            for (int b = 0; b < 64; b++) {
+                                if (othello.allowMoveOthello(b, player2.getPiece())) {
+                                    // player2
+                                    if (s == 0) {
+                                        AiOthello ai2 = new AiOthello(player2.getPlayernumber(), player2.getPiece());
+                                        int move2 = ai2.AiMove(othello, player2.getPiece(), ai4.indexOf(q) + 1);
+                                        othello.add(player2.getPiece(), move2);
+                                        othello.flipPiece(move2, player2.getPiece());
+                                        System.out.println(othello.toString());
+                                        break;
+                                    } else {
+                                        AiOthello ai2 = new AiOthello(player2.getPlayernumber(), player2.getPiece());
+                                        int move2 = ai2.AiMove(othello, player2.getPiece(), 9);
+                                        othello.add(player2.getPiece(), move2);
+                                        othello.flipPiece(move2, player2.getPiece());
+                                        System.out.println(othello.toString());
+                                        break;
+                                    }
+                                }
+                            }
+
+                            ArrayList<Integer> lijst = new ArrayList<>();
+                            for (int c = 0; c < 64; c++) {
+                                if (othello.allowMoveOthello(c, player1.getPiece()) || othello.allowMoveOthello(c, player2.getPiece())) {
+                                    lijst.add(0, 1);
+                                }
+                            }
+                            if (lijst.size() == 0) {
+                                break;
+                            }
+                        }
+
+                        Path fileName = Path.of("/ResultsAi/TestAivs" + q + ".txt"); //path van het bestand waarin de data wordt opgeslagen.
+
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName.toFile(), true));
+                        writer.append("\n");
+                        String ply1 = String.valueOf("Player: " + player1.getPlayernumber() + " Score is: " + othello.pieceCounter(player1.getPiece()));
+                        String ply2 = String.valueOf("Player: " + player2.getPlayernumber() + " Score is: " + othello.pieceCounter(player2.getPiece()));
+                        String result = ply1 + " || " + ply2 + " || nr_potje: " + potjes;
+
+                        if (othello.pieceCounter(player1.getPiece()) > othello.pieceCounter(player2.getPiece())) {
+                            player1win++;
+                        } else if (othello.pieceCounter(player1.getPiece()) == othello.pieceCounter(player2.getPiece())) {
+                            draw++;
+                        } else {
+                            player2win++;
+                        }
+
+                        if (potjes == 1) {
+                            writer.append("----------------------------------------------------------------");
+                            writer.append("\n");
+                            writer.append("Begin test");
+                            writer.append("\n");
+                            writer.append("\n");
+                            if (s == 0) {
+                                writer.append("Player 1: TestAi");
+                            } else {
+                                writer.append("Player 1: ").append(q);
+                            }
+                            writer.append("\n");
+                            if (s == 0) {
+                                writer.append("Player 2: ").append(q);
+                            } else {
+                                writer.append("Player 2: TestAi");
+                            }
+                            writer.append("\n");
+                            writer.append("Aantal potjes: 100");
+                            writer.append("\n");
+                            writer.append("Max depth: 4");
+                            writer.append("\n");
+                            writer.append("\n");
+                        }
+
+                        writer.append(result);
+
+                        if (potjes == 100) {
+                            writer.append("\n");
+                            writer.append("\n");
+                            writer.append("Player 1 total wins: ").append(String.valueOf(player1win));
+                            writer.append("\n");
+                            writer.append("Total draws: ").append(String.valueOf(draw));
+                            writer.append("\n");
+                            writer.append("Player 2 total wins: ").append(String.valueOf(player2win));
+                            writer.append("\n");
+                            writer.append("\n");
+                            writer.append("Einde test");
+                            writer.append("\n");
+                            writer.append("----------------------------------------------------------------");
+                            writer.append("\n");
+                        }
+
+                        writer.close();
+                    }
+                }
+            }
         }
     }
 }
